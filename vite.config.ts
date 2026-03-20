a09a07ef148009baeedb9ef4c6235aea5c98679e
jldfy <jaouad.lahlou.dfy@gmail.com>
2026-03-20 23:21:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fitmate-landing-page/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
